64d04c88099b544e96792e17a13476b8a881ab24
Riya <riyadedha642@gmail.com>
2024-09-06 13:52:42 +0530

    Update App.js

--- bookingapp/src/App.js
+++ bookingapp/src/App.js
@@ -6,7 +6,7 @@ import MainPage from './pages/main_page';
 import FormPage from './pages/main_form';
 import ConfirmationPage from './pages/main_confirm';
 import MovieCatalog from './components/MovieCatalog';
-import CreateMovie from './components/createmovie';
+// import CreateMovie from './components/createmovie';
 // import MovieList from './components/MovieList';
 import './styles/main.css';
 
